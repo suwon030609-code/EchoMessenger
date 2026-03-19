5f1d502418f51a35b9ec60fcc9b869a76ccda021
강준서 <suwon030609@gmail.com>
2026-03-19 16:45:44 +0900

    readme 추가

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            btnSend = new Button();
+            lblTitle = new Label();
+            lstChat = new ListBox();
+            txtMessage = new TextBox();
+            SuspendLayout();
+            // 
+            // btnSend
+            // 
+            btnSend.Location = new Point(1250, 823);
+            btnSend.Name = "btnSend";
+            btnSend.Size = new Size(150, 46);
+            btnSend.TabIndex = 0;
+            btnSend.Text = "전송";
+            btnSend.UseVisualStyleBackColor = true;
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.Location = new Point(200, 86);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(190, 32);
+            lblTitle.TabIndex = 1;
+            lblTitle.Text = "Echo Messenger";
+            // 
+            // lstChat
+            // 
+            lstChat.FormattingEnabled = true;
+            lstChat.Location = new Point(200, 156);
+            lstChat.Name = "lstChat";
+            lstChat.Size = new Size(1200, 580);
+            lstChat.TabIndex = 2;
+            // 
+            // txtMessage
+            // 
+            txtMessage.Location = new Point(200, 830);
+            txtMessage.Name = "txtMessage";
+            txtMessage.Size = new Size(200, 39);
+            txtMessage.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(14F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(1574, 929);
+            Controls.Add(txtMessage);
+            Controls.Add(lstChat);
+            Controls.Add(lblTitle);
+            Controls.Add(btnSend);
+            Name = "Form1";
+            Text = "Echo Messenger";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnSend;
+        private Label lblTitle;
+        private ListBox lstChat;
+        private TextBox txtMessage;
     }
 }
